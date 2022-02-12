911353bac5eddb7f06d2de829efaf220a4a9cd59
Heron Yang <heron.yang.tw@gmail.com>
2022-02-12 08:52:14 -0800

    Complete layout.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,13 @@ class Eldrow extends StatelessWidget {
     return MaterialApp(
       title: 'ELDROW - Wordle Solver',
       home: const MainPage(title: 'ELDROW'),
+      theme: new ThemeData(
+        primarySwatch: Colors.grey,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+              displayColor: Colors.white,
+            ),
+      ),
     );
   }
 }
@@ -26,13 +33,20 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  final List<TextEditingController> _clcpControllers =
+  final List<TextEditingController> _greenControllers =
       List.generate(5, (i) => TextEditingController());
+
+  // Dimension: position -> guess #.
+  final List<List<TextEditingController>> _yellowControllers =
+      List.generate(5, (i) => List.generate(4, (j) => TextEditingController()));
+
+  final List<TextEditingController> _greyControllers =
+      List.generate(20, (i) => TextEditingController());
 
   @override
   void dispose() {
-    for (int i = 0; i < _clcpControllers.length; i++) {
-      _clcpControllers[i].dispose();
+    for (int i = 0; i < _greenControllers.length; i++) {
+      _greenControllers[i].dispose();
     }
     super.dispose();
   }
@@ -42,7 +56,7 @@ class _MainPageState extends State<MainPage> {
       context: context,
       builder: (context) {
         return AlertDialog(
-          content: Text(_clcpControllers[1].text),
+          content: Text(_greenControllers[1].text),
         );
       },
     );
@@ -54,7 +68,17 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _buildCorrectLetterCorrectSpotInputTable(),
+      backgroundColor: Colors.black,
+      body: Column(children: [
+        SizedBox(height: 20),
+        Text('Position Matters'),
+        _buildCorrectLetterCorrectSpotInputTable(),
+        SizedBox(height: 20),
+        _buildCorrectLetterIncorrectSpotInputTable(),
+        SizedBox(height: 20),
+        Text('Position Doesn\'t Matter'),
+        _buildIncorrectLetterInputTable(),
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: _submit,
         tooltip: 'Submit',
@@ -69,14 +93,108 @@ class _MainPageState extends State<MainPage> {
         child: Table(
           border: TableBorder.all(),
           defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-          children: [
+          children: <TableRow>[
             TableRow(children: [
-              _buildLetterInput(_clcpControllers[0]),
-              _buildLetterInput(_clcpControllers[1]),
-              _buildLetterInput(_clcpControllers[2]),
-              _buildLetterInput(_clcpControllers[3]),
-              _buildLetterInput(_clcpControllers[4]),
+              _buildLetterInput(_greenControllers[0]),
+              _buildLetterInput(_greenControllers[1]),
+              _buildLetterInput(_greenControllers[2]),
+              _buildLetterInput(_greenControllers[3]),
+              _buildLetterInput(_greenControllers[4]),
             ]),
+          ],
+        ));
+  }
+
+  Widget _buildCorrectLetterIncorrectSpotInputTable() {
+    return Container(
+        color: Color(0xffb59f3b),
+        child: Table(
+          border: TableBorder.all(),
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: <TableRow>[
+            TableRow(
+              children: [
+                _buildLetterInput(_yellowControllers[0][0]),
+                _buildLetterInput(_yellowControllers[1][0]),
+                _buildLetterInput(_yellowControllers[2][0]),
+                _buildLetterInput(_yellowControllers[3][0]),
+                _buildLetterInput(_yellowControllers[4][0]),
+              ],
+            ),
+            TableRow(
+              children: [
+                _buildLetterInput(_yellowControllers[0][1]),
+                _buildLetterInput(_yellowControllers[1][1]),
+                _buildLetterInput(_yellowControllers[2][1]),
+                _buildLetterInput(_yellowControllers[3][1]),
+                _buildLetterInput(_yellowControllers[4][1]),
+              ],
+            ),
+            TableRow(
+              children: [
+                _buildLetterInput(_yellowControllers[0][2]),
+                _buildLetterInput(_yellowControllers[1][2]),
+                _buildLetterInput(_yellowControllers[2][2]),
+                _buildLetterInput(_yellowControllers[3][2]),
+                _buildLetterInput(_yellowControllers[4][2]),
+              ],
+            ),
+            TableRow(
+              children: [
+                _buildLetterInput(_yellowControllers[0][3]),
+                _buildLetterInput(_yellowControllers[1][3]),
+                _buildLetterInput(_yellowControllers[2][3]),
+                _buildLetterInput(_yellowControllers[3][3]),
+                _buildLetterInput(_yellowControllers[4][3]),
+              ],
+            ),
+          ],
+        ));
+  }
+
+  Widget _buildIncorrectLetterInputTable() {
+    return Container(
+        color: Color(0xff3a3a3c),
+        child: Table(
+          border: TableBorder.all(),
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: <TableRow>[
+            TableRow(
+              children: [
+                _buildLetterInput(_greyControllers[0]),
+                _buildLetterInput(_greyControllers[1]),
+                _buildLetterInput(_greyControllers[2]),
+                _buildLetterInput(_greyControllers[3]),
+                _buildLetterInput(_greyControllers[4]),
+              ],
+            ),
+            TableRow(
+              children: [
+                _buildLetterInput(_greyControllers[5]),
+                _buildLetterInput(_greyControllers[6]),
+                _buildLetterInput(_greyControllers[7]),
+                _buildLetterInput(_greyControllers[8]),
+                _buildLetterInput(_greyControllers[9]),
+              ],
+            ),
+            TableRow(
+              children: [
+                _buildLetterInput(_greyControllers[10]),
+                _buildLetterInput(_greyControllers[11]),
+                _buildLetterInput(_greyControllers[12]),
+                _buildLetterInput(_greyControllers[13]),
+                _buildLetterInput(_greyControllers[14]),
+              ],
+            ),
+            TableRow(
+              children: [
+                _buildLetterInput(_greyControllers[15]),
+                _buildLetterInput(_greyControllers[16]),
+                _buildLetterInput(_greyControllers[17]),
+                _buildLetterInput(_greyControllers[18]),
+                _buildLetterInput(_greyControllers[19]),
+              ],
+            ),
           ],
         ));
   }
